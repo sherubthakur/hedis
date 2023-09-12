d10e48dc41bef5a5bf38c58309a3b62ebe49d75a
Sherub Thakur <sherub.thakur@gmail.com>
2023-09-13 01:21:38 +0530

    Add expiry

--- app/Redis.hs
+++ app/Redis.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Redis where
@@ -11,18 +12,21 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader, ReaderT, ask)
 import Data.Bifunctor (first)
 import Data.ByteString (ByteString)
-import Data.ByteString.Char8 (putStrLn)
+import Data.ByteString.Char8 (putStrLn, readInteger)
 import Data.Map (Map)
 import Data.Map qualified as Map
+import Data.Time (UTCTime, diffUTCTime, getCurrentTime)
+import Data.Time.Clock (NominalDiffTime)
 import Network.Simple.TCP (Socket, recv, send)
 import RESP (Resp (..), decode, encode)
 import Text.Parsec (ParseError)
-import Utils (toBSSlow)
+import Utils (milliSecontsToNominalDiffTime, toBSSlow)
 import Prelude hiding (putStrLn)
 
 type Key = ByteString
 type Val = ByteString
-type StoredVal = ByteString
+data StoredVal = StoredVal
+    {value :: ByteString, storedWhen :: UTCTime, expiration :: Maybe NominalDiffTime}
 type Expiry = Integer
 
 type DB = TVar (Map Key StoredVal)
@@ -58,11 +62,15 @@ runCmd :: Resp -> Redis Resp
 runCmd (Array 1 [BulkStr "ping"]) = pure $ Str "PONG"
 runCmd (Array 2 [BulkStr "echo", BulkStr xs]) = pure $ Str xs
 runCmd (Array 3 [BulkStr "set", BulkStr k, BulkStr v]) = do
-    _ <- rInsert k v
+    _ <- rInsert k v Nothing
     pure $ Str "OK"
 runCmd (Array 2 [BulkStr "get", BulkStr k]) = do
     mval <- rLookup k
     pure $ maybe NullBulk Str mval
+runCmd (Array 5 [BulkStr "set", BulkStr k, BulkStr v, BulkStr "px", BulkStr t]) = do
+    let milliSeconds = fst <$> readInteger t
+    rInsert k v milliSeconds
+    pure $ Str "OK"
 runCmd _ = throwError $ UnimplementedError ""
 
 runCmdStr :: ByteString -> Redis ByteString
@@ -78,14 +86,25 @@ runCmdStr redisCmdStr = do
 newRedis :: IO DB
 newRedis = newTVarIO Map.empty
 
-rInsert :: Key -> Val -> Redis ()
-rInsert k value = do
+rInsert :: Key -> Val -> Maybe Expiry -> Redis ()
+rInsert k value maybeExpiry = do
     db <- ask
-    liftIO $ atomically $ modifyTVar db (Map.insert k value)
+    storedWhen <- liftIO getCurrentTime
+    let expiration = milliSecontsToNominalDiffTime <$> maybeExpiry
+    liftIO $ atomically $ modifyTVar db (Map.insert k StoredVal{..})
 
 rLookup :: Key -> Redis (Maybe Val)
 rLookup k = do
     db <- ask
+    currentTime <- liftIO getCurrentTime
     db <- liftIO $ readTVarIO db
-    let value = Map.lookup k db
+    let value = Map.lookup k db >>= getIfNotExpired currentTime
     pure value
+
+getIfNotExpired :: UTCTime -> StoredVal -> Maybe Val
+getIfNotExpired currentTime StoredVal{..} = case expiration of
+    Nothing -> Just value
+    Just exp ->
+        if currentTime `diffUTCTime` storedWhen < exp
+            then Just value
+            else Nothing
